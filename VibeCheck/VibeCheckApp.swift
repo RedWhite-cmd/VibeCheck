220673473d7e7e1cc012e42932295dc9d06f6032
Sudeep <sreddythatiparthi22@gmail.com>
2026-02-04 21:53:52 -0600

    Initial Commit

---
+++ VibeCheck/VibeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCheckApp.swift
+//  VibeCheck
+//
+//  Created by Sudeep Thatiparthi on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct VibeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
